d7ad53a900f0a3eab48a99756fe7d4dbd8b7dcf0
Ethan0104 <ethanw010404@gmail.com>
2025-04-06 23:01:27 -0400

    support input and textarea

--- better-double-click/src/contentScript.ts
+++ better-double-click/src/contentScript.ts
@@ -1,6 +1,8 @@
 'use strict'
 
+import { SKIP_IF_NO_CHANGE_IN_SELECTION } from './constants'
 import { getSurroundingInlineTextNodes } from './domUtils'
+import smartTextSelector from './selectorEngine'
 
 const parseDbClickSelectionGetSerializedTextNodeList = (
     selection: Selection
@@ -10,39 +12,48 @@ const parseDbClickSelectionGetSerializedTextNodeList = (
         console.error('.anchorNode is null', selection)
         return
     }
-    const startOffset = selection.anchorOffset
-    const endOffset = selection.focusOffset
 
-    // Note: we assume that the element is always a text node
-    if (textNode.nodeType !== Node.TEXT_NODE) {
+    // 2 cases here:
+    // 1. the element is a text node (regular text inbetween <p> for example)
+    // 2. something we can't handle (we already handled input and textarea in the upstream function)
+    if (textNode.nodeType === Node.TEXT_NODE) {
+        const startOffset = selection.anchorOffset
+        const endOffset = selection.focusOffset
+        console.log('startOffset', startOffset)
+        console.log('endOffset', endOffset)
+
+        const { inlineText, textNodes, initialNodeIndex } =
+            getSurroundingInlineTextNodes(textNode)
+
+        // while we have the context, we can also calculate the absolute start and end offsets
+        let absoluteStartOffset = 0
+        let absoluteEndOffset = 0
+        for (
+            let nodeCounter = 0;
+            nodeCounter < textNodes.length;
+            nodeCounter++
+        ) {
+            const node = textNodes[nodeCounter]
+            const nodeLength = node.nodeValue?.length ?? 0
+
+            if (nodeCounter < initialNodeIndex) {
+                absoluteStartOffset += nodeLength
+            }
+
+            if (nodeCounter === initialNodeIndex) {
+                absoluteStartOffset += startOffset
+                break
+            }
+        }
+        absoluteEndOffset = absoluteStartOffset + (endOffset - startOffset)
+
+        return { inlineText, textNodes, absoluteStartOffset, absoluteEndOffset }
+    } else {
         console.error(
             `Unexpected node type: ${textNode.nodeType}, please reach out to support.`
         )
         return
     }
-
-    const { inlineText, textNodes, initialNodeIndex } =
-        getSurroundingInlineTextNodes(textNode)
-
-    // while we have the context, we can also calculate the absolute start and end offsets
-    let absoluteStartOffset = 0
-    let absoluteEndOffset = 0
-    for (let nodeCounter = 0; nodeCounter < textNodes.length; nodeCounter++) {
-        const node = textNodes[nodeCounter]
-        const nodeLength = node.nodeValue?.length ?? 0
-
-        if (nodeCounter < initialNodeIndex) {
-            absoluteStartOffset += nodeLength
-        }
-
-        if (nodeCounter === initialNodeIndex) {
-            absoluteStartOffset += startOffset
-            break
-        }
-    }
-    absoluteEndOffset = absoluteStartOffset + (endOffset - startOffset)
-
-    return { inlineText, textNodes, absoluteStartOffset, absoluteEndOffset }
 }
 
 const getSerializedTextNodeList = (textNodes: Node[]) => {
@@ -54,7 +65,53 @@ const getSerializedTextNodeList = (textNodes: Node[]) => {
     })
 }
 
-document.addEventListener('dblclick', function (event) {
+document.addEventListener('dblclick', async function (event) {
+    // 2 cases here:
+    // 1. the element is a text node (regular text inbetween <p> for example)
+    // 2. the element is an input or textarea
+    // Let's do an early return for the 2nd case, signaled by event.target being an input or textarea
+    if (
+        event.target instanceof HTMLInputElement ||
+        event.target instanceof HTMLTextAreaElement
+    ) {
+        console.log('early return for input or textarea')
+        const element = event.target as HTMLInputElement | HTMLTextAreaElement
+        const start = element.selectionStart
+        const end = element.selectionEnd
+        if (start === null || end === null) {
+            console.error('start or end is null', start, end)
+            return
+        }
+        const inlineText = element.value
+
+        const smartTextSelectorResult = await smartTextSelector(
+            inlineText,
+            start,
+            end
+        )
+
+        // if no pattern is found, return
+        if (!smartTextSelectorResult) {
+            return
+        }
+
+        let { matchedStart, matchedEnd, matchedPattern } =
+            smartTextSelectorResult
+
+        // check if the selection is the same as the previous selection
+        if (
+            SKIP_IF_NO_CHANGE_IN_SELECTION &&
+            matchedStart == start &&
+            matchedEnd == end
+        ) {
+            return
+        }
+
+        // set the selection
+        element.setSelectionRange(matchedStart, matchedEnd)
+        return
+    }
+
     const selection = window.getSelection()
     if (!selection) {
         return
